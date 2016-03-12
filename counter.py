b545712c3a73dddc9f34dbf091742539d645268f
rebeccagettys <rebecca.gettys@students.olin.edu>
2016-03-12 13:36:24 -0500

    finished!

--- counter.py
+++ counter.py
@@ -2,7 +2,8 @@
 
 from os.path import exists
 import sys
-from pickle import dump, load
+#from pickle import dump, load
+import pickle
 
 def update_counter(file_name, reset=False):
 	""" Updates a counter stored in the file 'file_name'
@@ -29,11 +30,56 @@ def update_counter(file_name, reset=False):
 	>>> update_counter('blah2.txt')
 	2
 	"""
-	pass
+
+	if exists(file_name) == True: #if we have a file already
+		save_file = open(file_name, 'r')
+		counter = pickle.load(save_file) #get the counter
+		if reset == True: #reset the counter
+			counter = 1
+		else: #increment the counter
+			if 'counter' in locals(): #if it's already there
+				counter = counter + 1 # add one
+			else: #if it's not already there
+				counter = 1 # initialize
+
+
+		save_file = open(file_name, 'w')
+		pickle.dump(counter, save_file)
+		save_file.close()
+		return counter
+
+	else:
+		counter = 1
+		save_file = open(file_name, 'w')
+		pickle.dump(counter, save_file)
+		save_file.close()
+		return counter
+
+
+	# if reset == True:
+
+
+#			counter = 1
+#	else:
+#		if 'counter' in locals():
+#			counter = counter + 1
+#		else:
+#			counter = 1
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		import doctest
+
 		doctest.testmod()
 	else:
 		print "new value is " + str(update_counter(sys.argv[1]))
